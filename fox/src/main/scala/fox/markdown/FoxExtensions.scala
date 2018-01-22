696a46e29bcce35b8bbb123c8e7a770e8dc13e76
jvican <jorgevc@fastmail.es>
2018-01-22 15:33:46 +0100

    Add emoji and footnote extensions

--- fox/src/main/scala/fox/markdown/FoxExtensions.scala
+++ fox/src/main/scala/fox/markdown/FoxExtensions.scala
@@ -3,17 +3,21 @@ package fox.markdown
 import fox.Options
 import com.vladsch.flexmark.Extension
 import com.vladsch.flexmark.ext.autolink.AutolinkExtension
+import com.vladsch.flexmark.ext.emoji.EmojiExtension
+import com.vladsch.flexmark.ext.footnotes.FootnoteExtension
 import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension
 import com.vladsch.flexmark.ext.tables.TablesExtension
 
 object FoxExtensions {
   def default(options: Options): java.lang.Iterable[Extension] = {
     import scala.collection.JavaConverters._
-    Iterable(
+    List(
       AutolinkExtension.create(),
       FoxParserExtension.create(options),
       FoxAttributeProviderExtension.create(options),
       TablesExtension.create(),
+      EmojiExtension.create(),
+      FootnoteExtension.create(),
       StrikethroughExtension.create()
     ).asJava
   }
