30f8ecc1d002b60edcaa47e6a48d8fb473fde395
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-08-14 17:31:00 +0000

    Make it possible to pass in custom reporter to Main

--- vork/src/main/scala/vork/Main.scala
+++ vork/src/main/scala/vork/Main.scala
@@ -3,33 +3,28 @@ package vork
 import java.io.PrintStream
 import java.nio.file.Path
 import metaconfig.Configured
-import scala.meta.inputs.Position
 import scala.meta.internal.io.PathIO
 import scala.meta.io.AbsolutePath
-import scala.tools.nsc.interpreter.OutputStream
-import vork.internal.cli.Settings
 import vork.internal.cli.MainOps
+import vork.internal.cli.Settings
 import vork.internal.io.ConsoleReporter
 import vork.internal.markdown.Markdown
 
 object Main {
+
   def main(args: Array[String]): Unit = {
-    sys.exit(process(args, System.out, PathIO.workingDirectory.toNIO))
+    val code = process(args, System.out, PathIO.workingDirectory.toNIO)
+    sys.exit(code)
   }
 
-  def process(
-      args: Array[String],
-      stdout: OutputStream,
-      cwd: Path
-  ): Int = {
-    val out = new PrintStream(stdout)
-    val reporter = new ConsoleReporter(out)
+  def process(args: Array[String], out: PrintStream, cwd: Path): Int = {
+    process(args, new ConsoleReporter(out), cwd)
+  }
+
+  def process(args: Array[String], reporter: Reporter, cwd: Path): Int = {
     val base = Settings.default(AbsolutePath(cwd))
     Settings.fromCliArgs(args.toList, reporter, base) match {
       case Configured.NotOk(error) =>
-        pprint.log(error)
-        pprint.log(error.all)
-        reporter.error("foooo")
         error.all.foreach(message => reporter.error(message))
         1
       case Configured.Ok(context) =>
@@ -39,7 +34,6 @@ object Main {
         if (context.reporter.hasErrors) {
           1 // error
         } else {
-          out.println(context.settings.out.toString)
           0
         }
     }
