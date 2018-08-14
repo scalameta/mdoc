0954eba6aba61295f9c4040b0816e3dd6c571ab6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-08-14 14:19:15 +0000

    Make stuff private

--- vork/src/main/scala/vork/Reporter.scala
+++ vork/src/main/scala/vork/Reporter.scala
@@ -3,13 +3,14 @@ package vork
 import scala.meta.inputs.Position
 
 trait Reporter {
-  def hasErrors: Boolean
-  def reset(): Unit
   def error(pos: Position, throwable: Throwable): Unit
   def error(pos: Position, msg: String): Unit
   def error(msg: String): Unit
-  def info(pos: Position, msg: String): Unit
-  def info(msg: String): Unit
   def warning(pos: Position, msg: String): Unit
   def warning(msg: String): Unit
+  def info(pos: Position, msg: String): Unit
+  def info(msg: String): Unit
+
+  private[vork] def hasErrors: Boolean
+  private[vork] def reset(): Unit
 }
