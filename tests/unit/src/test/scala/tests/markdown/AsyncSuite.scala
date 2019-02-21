44a85d746a6df3e6e8fc0e3f367af24cd7ab18d6
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-02-21 09:59:27 +0100

    Update expected output for 2.12

--- tests/unit/src/test/scala/tests/markdown/AsyncSuite.scala
+++ tests/unit/src/test/scala/tests/markdown/AsyncSuite.scala
@@ -31,15 +31,33 @@ class AsyncSuite extends BaseMarkdownSuite {
        |java.lang.ExceptionInInitializerError
        |	at repl.Session$.app(timeout.md:3)
        |Caused by: java.util.concurrent.TimeoutException: Futures timed out after [10 milliseconds]
-       |	at scala.concurrent.impl.Promise$DefaultPromise.ready(Promise.scala:223)
-       |	at scala.concurrent.impl.Promise$DefaultPromise.result(Promise.scala:227)
-       |	at scala.concurrent.Await$$anonfun$result$1.apply(package.scala:190)
-       |	at scala.concurrent.BlockContext$DefaultBlockContext$.blockOn(BlockContext.scala:53)
-       |	at scala.concurrent.Await$.result(package.scala:190)
+       |	at scala.concurrent.impl.Promise$DefaultPromise.ready(Promise.scala:259)
+       |	at scala.concurrent.impl.Promise$DefaultPromise.result(Promise.scala:263)
+       |	at scala.concurrent.Await$.$anonfun$result$1(package.scala:219)
+       |	at scala.concurrent.BlockContext$DefaultBlockContext$.blockOn(BlockContext.scala:57)
+       |	at scala.concurrent.Await$.result(package.scala:146)
        |	at repl.Session$App$.<init>(timeout.md:11)
        |	at repl.Session$App$.<clinit>(timeout.md)
        |	... 1 more
+    """.stripMargin,
+    compat = Map(
+      "2.11" ->
+        """|error: timeout.md:4:1: Futures timed out after [10 milliseconds]
+           |Await.result(Future(Thread.sleep(1000)), Duration("10ms"))
+           |^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+           |java.lang.ExceptionInInitializerError
+           |	at repl.Session$.app(timeout.md:3)
+           |Caused by: java.util.concurrent.TimeoutException: Futures timed out after [10 milliseconds]
+           |	at scala.concurrent.impl.Promise$DefaultPromise.ready(Promise.scala:223)
+           |	at scala.concurrent.impl.Promise$DefaultPromise.result(Promise.scala:227)
+           |	at scala.concurrent.Await$$anonfun$result$1.apply(package.scala:190)
+           |	at scala.concurrent.BlockContext$DefaultBlockContext$.blockOn(BlockContext.scala:53)
+           |	at scala.concurrent.Await$.result(package.scala:190)
+           |	at repl.Session$App$.<init>(timeout.md:11)
+           |	at repl.Session$App$.<clinit>(timeout.md)
+           |	... 1 more
     """.stripMargin
+    )
   )
 
   check(
