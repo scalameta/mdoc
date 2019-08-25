f0126ff373bcff55a97b45a7ed45fdd7a179cb3c
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-08-25 16:38:29 -0700

    Add test-release.sh script

---
+++ bin/test-release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eux
+
+version=$1
+argumentsRest=${@:2}
+suffix=${argumentsRest:-}
+
+coursier fetch \
+  org.scalameta:mdoc_2.11:$version \
+  org.scalameta:mdoc_2.12:$version \
+  org.scalameta:mdoc_2.13:$version \
+  org.scalameta:mdoc-js_2.11:$version \
+  org.scalameta:mdoc-js_2.12:$version \
+  org.scalameta:mdoc-js_2.13:$version $suffix
+
+coursier fetch \
+    "org.scalameta:sbt-mdoc;sbtVersion=1.0;scalaVersion=2.12:$version" \
+    --sbt-plugin-hack $suffix
