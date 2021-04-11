4f49476c207372670f63110734fa30a7dd4d5c3b
Anton Sviridov <keynmol@gmail.com>
2021-04-11 14:26:07 +0100

    Fix BuildInfo

--- tests/unit/src/test/scala/tests/cli/MdocPropertiesSuite.scala
+++ tests/unit/src/test/scala/tests/cli/MdocPropertiesSuite.scala
@@ -3,13 +3,12 @@ package tests.cli
 import mdoc.internal.cli.MdocProperties
 import munit.FunSuite
 import scala.meta.internal.io.PathIO
-import mdoc.internal.BuildInfo
 
 class MdocPropertiesSuite extends FunSuite {
   test("default") {
     val obtained = MdocProperties.default(PathIO.workingDirectory).scalacOptions
 
-    if (BuildInfo.scalaVersion.startsWith("3.0"))
+    if (mdoc.internal.BuildInfo.scalaVersion.startsWith("3.0"))
       assert(obtained.contains("-language:implicitConversions"))
     else
       assert(obtained.contains("-deprecation"))
